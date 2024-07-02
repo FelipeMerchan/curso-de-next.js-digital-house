0eba74c8cb5787fcde6ccc399348b74ebfc8e900
Felipe Merchan <felipemerchanfuquen@gmail.com>
2024-07-01 21:55:52 -0500

    8. Instalación de Storybook + Tailwind CSS + Next.js

---
+++ src/stories/messages/Message.stories.ts
@@ -0,0 +1,26 @@
+import { Message } from '@/components/messages/Message';
+import type { Meta, StoryObj } from '@storybook/react';
+
+const meta = {
+  title: 'Messages/Message',
+  component: Message,
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
+    layout: 'centered',
+  },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ['autodocs'],
+} satisfies Meta<typeof Message>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: {
+    message: {
+      message: 'Mensaje de prueba',
+      name: 'Anakin Skywalker',
+      username: 'anakin', 
+    },
+  },
+};
